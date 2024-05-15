698e8114cc7b2d6c5ce213cbc2a50626a3b87211
zhangpaopao <zhangpaopao0609@gmail.com>
2024-05-15 18:44:41 +0800

    feat: again

--- server/api/getAllHtml.ts
+++ server/api/getAllHtml.ts
@@ -1,25 +1,62 @@
-import { CheerioCrawler, KeyValueStore } from 'crawlee';
+import { CheerioCrawler, PuppeteerCrawler, KeyValueStore } from 'crawlee';
 import * as cheerio from 'cheerio';
 
 async function sleep(ms: number) {
   await new Promise(resolve => setTimeout(resolve, ms))
 }
 
+// async function getEachSchoolHtml(url: string, idx: number) {
+//   const crawler = new CheerioCrawler({
+//     maxRequestRetries: 5,
+//     async requestHandler({ request, response, body, contentType, $, }) {
+//       await KeyValueStore.setValue(`school-${idx}`, body);
+//     },
+//   });
+
+//   await crawler.run([url]);
+// }
+
 async function getEachSchoolHtml(url: string, idx: number) {
-  const crawler = new CheerioCrawler({
-    async requestHandler({ request, response, body, contentType, $, }) {
-      await KeyValueStore.setValue(`school-${idx}`, body);
+  const crawler = new PuppeteerCrawler({
+    maxRequestRetries: 5,
+    async requestHandler({ request, page }) {
+      const content = await page.content()
+      await KeyValueStore.setValue(`school-${idx}`, content);
     },
   });
 
   await crawler.run([url]);
 }
 
+// async function getAllHtml() {
+//   const schoolsLink: string[] = [];
+//   const crawler = new CheerioCrawler({
+//     async requestHandler({ request, response, body, contentType, $, }) {
+//       await KeyValueStore.setValue('main', body);
+//       const schoolNameHtml = $('.card-row-title.school-name');
+//       schoolNameHtml.each(async (idx, ele) => {
+//         const a =  $(ele).find('a')
+//         schoolsLink.push(a.attr('href') || '')
+//       })
+//     },
+//   });
+
+//   await crawler.run(['https://www.international-schools-database.com/in/kuala-lumpur']);
+//   // await crawler.run(['https://www.mi.com/']);
+
+//   schoolsLink.forEach(async (schoolLink, index) => {
+//     await getEachSchoolHtml(schoolLink, index)
+//     await sleep(1000 * 60 * 10)
+//   })
+// }
+
 async function getAllHtml() {
   const schoolsLink: string[] = [];
-  const crawler = new CheerioCrawler({
-    async requestHandler({ request, response, body, contentType, $, }) {
-      await KeyValueStore.setValue('main', body);
+  const crawler = new PuppeteerCrawler({
+    async requestHandler({ request, page }) {
+      const content = await page.content()
+      await KeyValueStore.setValue('main', content);
+      const $ = cheerio.load(content)
       const schoolNameHtml = $('.card-row-title.school-name');
       schoolNameHtml.each(async (idx, ele) => {
         const a =  $(ele).find('a')
@@ -33,11 +70,15 @@ async function getAllHtml() {
 
   schoolsLink.forEach(async (schoolLink, index) => {
     await getEachSchoolHtml(schoolLink, index)
-    await sleep(1000)
+    await sleep(1000 * 60 * 10)
   })
 }
 
 
 export default defineEventHandler(async (event) => {
-  await getAllHtml()
+  getAllHtml()
+
+  return {
+    code: 0
+  }
 })
