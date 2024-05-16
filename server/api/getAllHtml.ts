dceb7ea2ad1d3909e37934c14e33482f3d5ca222
zhangpaopao <zhangpaopao0609@gmail.com>
2024-05-16 17:27:58 +0800

    feat: filter

--- server/api/getAllHtml.ts
+++ server/api/getAllHtml.ts
@@ -1,5 +1,7 @@
 import { CheerioCrawler, PuppeteerCrawler, KeyValueStore } from 'crawlee';
 import * as cheerio from 'cheerio';
+import { existsSync  } from "node:fs";
+import { resolve  } from "node:path";
 
 async function sleep(ms: number) {
   await new Promise(resolve => setTimeout(resolve, ms))
@@ -27,7 +29,23 @@ async function getEachSchoolHtml(url: string, idx: number) {
 
 //   await crawler.run([url]);
 // }
-
+// const htmlIndex = [
+//   0,1,2,3,4,5,6,7,8,9,10,
+//   11,12,13,14,15,16,17,18,19,20,
+//   21,22,23,24,25,26,27,28,29,30,
+//   31,32,33,34,35,36,37,38,39,40,
+//   41,42,43,44,45,46,47,48,49,50,
+//   51,52,53,54,55,56,57,58,59,60,
+//   61,62,63,64,65,66,67,68,69,70,
+//   71,72,73,74,75,76,77,78,79,80,
+//   81,82,83,84,85,86,87,88,89,90,
+//   91,92,93,94,95,96,97,98,99,100,
+//   101,102,103,104,105,106,107,108,109,110,
+//   111,112,113,114,115,116,117,118,119,120,
+//   121,122,123,124,125,126,127,128,129,130,
+//   131,132,133,134,135,136,137,138,139,140,
+//   141,142,143
+// ]
 async function getAllHtml() {
   const schoolsLink: string[] = [];
   const crawler = new CheerioCrawler({
@@ -45,6 +63,9 @@ async function getAllHtml() {
   // await crawler.run(['https://www.mi.com/']);
 
   for (let i = 0; i < schoolsLink.length; i++) {
+    if(existsSync(resolve(__dirname, '../..', 'storage', 'key_value_stores', 'default', `school-${i}.json`))) {
+      continue
+    }
     const schoolLink = schoolsLink[i];
     await getEachSchoolHtml(schoolLink, i)
     await sleep(1000 * 60 * 10)
