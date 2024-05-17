68e2c50e83e83934143fcb9af5ae58e96f0989c2
zhangpaopao <zhangpaopao0609@gmail.com>
2024-05-17 17:02:33 +0800

    feat: ok

--- server/api/page.ts
+++ server/api/page.ts
@@ -3,29 +3,8 @@ import { existsSync, readFileSync  } from "node:fs";
 import { resolve, dirname  } from "node:path";
 
 export interface School {
-  name: string,
-  curriculum: string,
-  language: string,
-  ages: string,
-  fees?: string[],
-  feesUnit: string,
-  link: string,
-  city: string,
-  isNativeEnglishTeachers: string,
-  extraEnglish: string,
-  classSizes: {
-    average: string,
-    maximum: string,
-  },
-  extraCurricularActivities: string,
-  schoolBus: string,
+  [key: string]: string | Record<string, string>,
 }
-
-async function sleep(ms: number) {
-  await new Promise(resolve => setTimeout(resolve, ms))
-}
-
-
 
 export default defineEventHandler(async (event) => {
   const schools: Partial<School>[] = []
@@ -43,24 +22,6 @@ export default defineEventHandler(async (event) => {
       schools[idx].name = a.text()
       schools[idx].link = a.attr('href')
     })
-  
-    const schoolBasicInfoHtml = $('.card-row-properties > dl');
-  
-    schoolBasicInfoHtml.each((idx, ele) => {
-      const dd = $(ele).find('dd')
-      const dt = $(ele).find('dt')
-      if(!schools[idx]) {
-        schools[idx] = {};
-      }
-  
-      schools[idx].curriculum = $(dt[0]).text()
-      schools[idx].language = $(dt[1]).text()
-      schools[idx].ages = $(dt[2]).text();
-      const currency = $(dt[3]).find('.currency');
-      if(currency) {
-        schools[idx].fees = [$(currency[0]).text(), $(currency[1]).text()];
-      }
-    })
   }
 
   getMain()
@@ -68,18 +29,35 @@ export default defineEventHandler(async (event) => {
 
   for (let idx = 0; idx < schools.length; idx++) {
     const school = schools[idx];
-    const $ = cheerio.load(readFileSync(resolve(dirname('.'), 'schools', `school-${idx}.json`), 'utf-8'));
-    const infoHtml = $('.working-hours.detail-description');
-    school.city = $($(infoHtml).find('.day.clearfix')[0]).find('.hours').text();
-    const tableTrHtml = $('.detail-summary > tbody > tr');
-    school.isNativeEnglishTeachers = $($(tableTrHtml)[1]).find('td').html() || ''
-    school.extraEnglish = $($(tableTrHtml)[2]).find('td').html() || ''
-    school.classSizes = {
-      average: $($(tableTrHtml)[3]).find('td').text() || '',
-      maximum: $($(tableTrHtml)[4]).find('td').text() || '',
+    if(!existsSync(resolve(dirname('.'), 'schools', `school-${idx}.json`))) {
+      continue
     }
-    school.extraCurricularActivities = $($(tableTrHtml)[5]).find('td').html() || ''
-    school.schoolBus = $($(tableTrHtml)[6]).find('td').html() || ''
+    const $ = cheerio.load(readFileSync(resolve(dirname('.'), 'schools', `school-${idx}.json`), 'utf-8'));
+
+    const infoHtml = $('.working-hours.detail-description');
+    const infoHtmlItems = $(infoHtml).find('.day.clearfix')
+    infoHtmlItems.each((idx, item) => {
+      const key = $(item).find('.name').text().trim();
+      let value = $(item).find('.hours').text().trim();
+
+      if($(item).find('.hours').find('a').length) {
+        value = $(item).find('.hours').find('a').attr('href') || ''
+      }
+      school[key] = value;
+    })
+
+    const panelGroup = $('.panel-group > .panel.panel-default');
+    panelGroup.each((idx, item) => {
+      const obj: Record<string, string> = {};
+      const title = $(item).find('.panel-title').find('a').text().trim();
+      const tableTr = $(item).find('.panel-collapse .table  tr')
+      tableTr.each((i, tr) => {
+        const k = $(tr).find('.question').text().trim();
+        const v = $(tr).find('.answer').text().trim();
+        obj[k] = v
+      })
+      school[title] = obj
+    })
   }
 
   return {
