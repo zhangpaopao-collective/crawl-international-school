e3fd340bf1d6d3a10ae096b2e5cb871d075ccf98
zhangpaopao <zhangpaopao0609@gmail.com>
2024-05-17 13:51:40 +0800

    feat: better

--- server/api/page.ts
+++ server/api/page.ts
@@ -30,60 +30,57 @@ async function sleep(ms: number) {
 export default defineEventHandler(async (event) => {
   const schools: Partial<School>[] = []
 
-  function getMain() {
-    const $ = cheerio.load(readFileSync(resolve(dirname('.'), 'schools', 'main.json')), );
-    // console.log(readFileSync(resolve(dirname('.'), 'schools', 'main.json'), 'utf-8'));
+   function getMain() {
+    const $ = cheerio.load(readFileSync(resolve(dirname('.'), 'schools', 'main.json')));
     
-    const schoolNameHtml = $('span.icon-bar');
+    const schoolNameHtml = $('.card-row-title.school-name');
 
-    console.log(schoolNameHtml);
-
-    // schoolNameHtml.each((idx, ele) => {
-    //   const a =  $(ele).find('a')
-    //   if(!schools[idx]) {
-    //     schools[idx] = {};
-    //   }
-    //   schools[idx].name = a.text()
-    //   schools[idx].link = a.attr('href')
-    // })
+    schoolNameHtml.each((idx, ele) => {
+      const a =  $(ele).find('a')
+      if(!schools[idx]) {
+        schools[idx] = {};
+      }
+      schools[idx].name = a.text()
+      schools[idx].link = a.attr('href')
+    })
   
-    // const schoolBasicInfoHtml = $('.card-row-properties > dl');
+    const schoolBasicInfoHtml = $('.card-row-properties > dl');
   
-    // schoolBasicInfoHtml.each((idx, ele) => {
-    //   const dd = $(ele).find('dd')
-    //   const dt = $(ele).find('dt')
-    //   if(!schools[idx]) {
-    //     schools[idx] = {};
-    //   }
+    schoolBasicInfoHtml.each((idx, ele) => {
+      const dd = $(ele).find('dd')
+      const dt = $(ele).find('dt')
+      if(!schools[idx]) {
+        schools[idx] = {};
+      }
   
-    //   schools[idx].curriculum = $(dt[0]).text()
-    //   schools[idx].language = $(dt[1]).text()
-    //   schools[idx].ages = $(dt[2]).text();
-    //   const currency = $(dt[3]).find('.currency');
-    //   if(currency) {
-    //     schools[idx].fees = [$(currency[0]).text(), $(currency[1]).text()];
-    //   }
-    // })
+      schools[idx].curriculum = $(dt[0]).text()
+      schools[idx].language = $(dt[1]).text()
+      schools[idx].ages = $(dt[2]).text();
+      const currency = $(dt[3]).find('.currency');
+      if(currency) {
+        schools[idx].fees = [$(currency[0]).text(), $(currency[1]).text()];
+      }
+    })
   }
 
   getMain()
 
 
-  // for (let idx = 0; idx < schools.length; idx++) {
-  //   const school = schools[idx];
-  //   const $ = cheerio.load(readFileSync(resolve(dirname('.'), 'schools', `school-${idx}.json`), 'utf-8'));
-  //   const infoHtml = $('.working-hours.detail-description');
-  //   school.city = $($(infoHtml).find('.day.clearfix')[0]).find('.hours').text();
-  //   const tableTrHtml = $('.detail-summary > tbody > tr');
-  //   school.isNativeEnglishTeachers = $($(tableTrHtml)[1]).find('td').html() || ''
-  //   school.extraEnglish = $($(tableTrHtml)[2]).find('td').html() || ''
-  //   school.classSizes = {
-  //     average: $($(tableTrHtml)[3]).find('td').text() || '',
-  //     maximum: $($(tableTrHtml)[4]).find('td').text() || '',
-  //   }
-  //   school.extraCurricularActivities = $($(tableTrHtml)[5]).find('td').html() || ''
-  //   school.schoolBus = $($(tableTrHtml)[6]).find('td').html() || ''
-  // }
+  for (let idx = 0; idx < schools.length; idx++) {
+    const school = schools[idx];
+    const $ = cheerio.load(readFileSync(resolve(dirname('.'), 'schools', `school-${idx}.json`), 'utf-8'));
+    const infoHtml = $('.working-hours.detail-description');
+    school.city = $($(infoHtml).find('.day.clearfix')[0]).find('.hours').text();
+    const tableTrHtml = $('.detail-summary > tbody > tr');
+    school.isNativeEnglishTeachers = $($(tableTrHtml)[1]).find('td').html() || ''
+    school.extraEnglish = $($(tableTrHtml)[2]).find('td').html() || ''
+    school.classSizes = {
+      average: $($(tableTrHtml)[3]).find('td').text() || '',
+      maximum: $($(tableTrHtml)[4]).find('td').text() || '',
+    }
+    school.extraCurricularActivities = $($(tableTrHtml)[5]).find('td').html() || ''
+    school.schoolBus = $($(tableTrHtml)[6]).find('td').html() || ''
+  }
 
   return {
     schools
